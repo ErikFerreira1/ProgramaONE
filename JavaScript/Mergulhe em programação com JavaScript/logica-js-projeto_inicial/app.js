a81c4336474868915daa8d075e5c353548d09c25
Erik Ferreira <136654783+ErikFerreira1@users.noreply.github.com>
2024-01-23 20:23:22 -0300

    .

--- JavaScript/Mergulhe em programação com JavaScript/logica-js-projeto_inicial/app.js
+++ JavaScript/Mergulhe em programação com JavaScript/logica-js-projeto_inicial/app.js
@@ -1,8 +1,22 @@
-alert("Seja bem vindo ao jogo!")
-let NumSecret = 3
-let NumChoice = prompt("Digite um número:")
-if (NumChoice == 3) {
-    alert(`Parabéns você acertou o número. Você escolheu ${NumChoice} e o número era ${NumSecret}`)
-}else {
-    alert("Você Errou")
+alert("Seja bem vindo ao jogo!");
+let NumSecret = 3;
+let tries = 1;
+let NumChoice;
+
+while (NumChoice != NumSecret) {
+  NumChoice = prompt("Escolha um número de 1 a 10");
+  if (NumChoice == 3) {
+    break
+  } else {
+    if(NumChoice > NumSecret){
+        alert(`O número secreto é menor que ${NumChoice}`)
+    }else{
+        alert(`O número secreto é maior que ${NumChoice}`)
+    };
+    tries++;
+  }
 }
+let wordChoice = tries > 1 ? "tentativas" : "tentativa"
+alert(
+  `Parabéns você acertou o número. Você escolheu ${NumChoice} e o número era ${NumSecret}. ${tries} ${wordChoice}`
+);
