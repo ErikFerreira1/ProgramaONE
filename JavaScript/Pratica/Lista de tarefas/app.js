d370cc5839704e6fcbe24b19fce1c7387015e36a
Erik Ferreira <136654783+ErikFerreira1@users.noreply.github.com>
2024-02-28 20:57:18 -0300

    Update app.js

--- JavaScript/Pratica/Lista de tarefas/app.js
+++ JavaScript/Pratica/Lista de tarefas/app.js
@@ -1,26 +1,40 @@
-let lista = []
+let lista = [];
 
 function Adicionar() {
   let valorTarefa = document.getElementById("novaTarefa").value;
   let listaTarefa = document.getElementById("listaTarefa");
-  let itemLista = document.createElement("li")
-
+  
   if (lista.includes(valorTarefa) == false) {
-    lista.push(valorTarefa)
-    itemLista.innerText = valorTarefa
-    listaTarefa.appendChild(itemLista)
-  }else {
-    alert("O item já está na lista")
+    lista.push(valorTarefa);
+
+    // Adicionar item
+    let itemLista = document.createElement("li");
+    itemLista.innerText = valorTarefa;
+    listaTarefa.appendChild(itemLista);
+
+    // Botão de exclusão
+    let deleteItem = document.createElement("button");
+    deleteItem.innerText = "x";
+    itemLista.appendChild(deleteItem);
+  
+    deleteItem.addEventListener("click", function delItem() {
+      let removerItemLista = lista.indexOf(valorTarefa)
+      if(removerItemLista != -1) {
+        lista.splice(removerItemLista, 1)
+        listaTarefa.removeChild(itemLista)
+      }
+      
+    });
+
+  } else {
+    alert("O item já está na lista");
   }
-  
-  console.log(lista)
-
-  
+  console.log(lista);
 }
+
 function Limpar() {
-  let limparLista = document.getElementById("listaTarefa")
-  limparLista.innerHTML = ""
-  lista = []
-  console.log(lista)
+  let limparLista = document.getElementById("listaTarefa");
+  limparLista.innerHTML = "";
+  lista = [];
+  console.log(lista);
 }
-
