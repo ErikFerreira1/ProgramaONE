9e967b8b06b591b3900a3a21643b6566eeca6691
Erik Ferreira <136654783+ErikFerreira1@users.noreply.github.com>
2024-03-06 23:41:00 -0300

    .

--- JavaScript/Pratica/Filmin/app.js
+++ JavaScript/Pratica/Filmin/app.js
@@ -1,21 +1,26 @@
-function clicou() {
-  async function filmin() {
-    let nameMovie = document.getElementById("valorfilme").value;
+async function buscarFilme() {
+  const nameMovie = document.getElementById("valorfilme").value;
+  const tituloFilme = document.getElementById("titulo");
+  const anoFilme = document.getElementById("anoFilme");
+  const imagemFilme = document.getElementById("imagemFilme");
 
-    try {
-      const response = await fetch(
-        `http://www.omdbapi.com/?apikey=7c667d40&t=${nameMovie}&s=${nameMovie}`
-      );
-      if (!response.ok) {
-        throw new error("Error ao carregar dados!");
-      }
-      const data = await response.json();
-      const filme = JSON.stringify(data);
-      console.log(filme);
-    } catch (error) {
-      console.error("Ocorreu um erro", error);
+  try {
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=7c667d40&t=${nameMovie}&s=${nameMovie}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Erro ao carregar dados!");
     }
+
+    const data = await response.json();
+    const primeiroFilme = data.Search[0];
+
+    tituloFilme.innerText = primeiroFilme.Title;
+    anoFilme.innerText = primeiroFilme.Year;
+    imagemFilme.src = primeiroFilme.Poster;
+  } catch (error) {
+    console.error("Ocorreu um erro", error);
   }
-  filmin();
+  
 }
-//7c667d40
