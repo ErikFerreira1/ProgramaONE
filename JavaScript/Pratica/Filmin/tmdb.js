7527c8e69b01560f1980804c06d407d5bf62c786
Erik Ferreira <136654783+ErikFerreira1@users.noreply.github.com>
2024-03-18 19:20:40 -0300

    .

--- JavaScript/Pratica/Filmin/tmdb.js
+++ JavaScript/Pratica/Filmin/tmdb.js
@@ -8,35 +8,43 @@ async function buscarFilme() {
     alert("Por favor, preencha todos os campos!");
   } else {
     try {
-      const url = `https://www.omdbapi.com/?apikey=7c667d40&t=${nameMovie}&s=${nameMovie}&type=${valueSelect}`;
+      const apiKey = "f7618a55c1d648cc00383ed3b123cffe";
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${nameMovie}&include_adult=false`;
+
       const response = await fetch(url);
 
       if (!response.ok) {
         throw new Error("Erro ao carregar dados!");
       }
+
       const data = await response.json();
 
-      if (!data.Search || data.Search.length === 0) {
+      if (data.results.length === 0) {
         alert("Nenhum resultado encontrado!");
       }
 
-      for (const filme of data.Search) {
+      for (const filme of data.results) {
         const movieInfo = document.createElement("div");
         movieInfo.classList.add("movie-info");
 
         const imagemFilme = document.createElement("img");
-        imagemFilme.src =
-          filme.Poster != "N/A" ? filme.Poster : "./image/noimage.jpg";
+        imagemFilme.src = filme.poster_path
+          ? `https://image.tmdb.org/t/p/w500/${filme.poster_path}`
+          : "./image/noimage.jpg";
 
         const tituloFilme = document.createElement("h1");
-        tituloFilme.innerText = filme.Title;
+        tituloFilme.innerText = filme.title;
 
         const anoFilme = document.createElement("p");
-        anoFilme.innerText = filme.Year;
+        anoFilme.innerText = filme.release_date.substring(0, 4);
+
+        const notaFilme = document.createElement("p");
+        notaFilme.innerText = `Nota: ${filme.vote_average}`;
 
         movieInfo.appendChild(imagemFilme);
         movieInfo.appendChild(tituloFilme);
         movieInfo.appendChild(anoFilme);
+        movieInfo.appendChild(notaFilme);
         infos.appendChild(movieInfo);
       }
     } catch (error) {
@@ -44,4 +52,3 @@ async function buscarFilme() {
     }
   }
 }
-// key f7618a55c1d648cc00383ed3b123cffe
