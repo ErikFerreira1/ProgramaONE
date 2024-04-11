7bcd73559aea63cb5d810c2e5e5fd9a45e803e36
Erik Ferreira <136654783+ErikFerreira1@users.noreply.github.com>
2024-04-11 17:57:24 -0300

    .

--- JavaScript/Pratica/Desing Filmin/components/Search/search.js
+++ JavaScript/Pratica/Desing Filmin/components/Search/search.js
@@ -1,9 +1,19 @@
+function openSearch() {
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+
 function getParameterByName(name, url) {
   if (!url) url = window.location.href;
   name = name.replace(/[\[\]]/g, "\\$&");
   var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
     results = regex.exec(url);
-  if (!results) return null;
+  if (!results) return "";
   if (!results[2]) return "";
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
