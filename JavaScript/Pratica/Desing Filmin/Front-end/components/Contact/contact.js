b2c69f11b04bd135b5f9f133a6a16742526868bc
Erik Ferreira <136654783+ErikFerreira1@users.noreply.github.com>
2024-06-02 20:42:03 -0300

    .

--- JavaScript/Pratica/Desing Filmin/Front-end/components/Contact/contact.js
+++ JavaScript/Pratica/Desing Filmin/Front-end/components/Contact/contact.js
@@ -5,7 +5,108 @@ function openSearch() {
 function closeSearch() {
   document.getElementById("myOverlay").style.display = "none";
 }
+
 document.addEventListener("DOMContentLoaded", function () {
+  const apiKey = "f7618a55c1d648cc00383ed3b123cffe";
+  const genreSelect = document.getElementById("genre");
+  const form = document.getElementById("searchForm");
+  const resultsDiv = document.getElementById("results");
+  const imageBaseUrl = "https://image.tmdb.org/t/p/w500";
+
+  async function getGenres() {
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}&language=pt-BR`
+      );
+      const data = await response.json();
+      const genres = data.genres;
+      genres.forEach((genre) => {
+        const option = document.createElement("option");
+        option.value = genre.id;
+        option.textContent = genre.name;
+        genreSelect.appendChild(option);
+      });
+    } catch (error) {
+      console.error("Erro ao obter gêneros:", error);
+    }
+  }
+
+  async function searchMovies(event) {
+    event.preventDefault();
+    const genre = genreSelect.value;
+    const minDuration = 60;
+    const releaseYear = document.getElementById("releaseYear").value;
+
+    let url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=pt-BR&with_genres=${genre}&sort_by=popularity.desc`;
+
+    if (releaseYear) {
+      const currentYear = new Date().getFullYear();
+
+      url += `&primary_release_date.gte=${releaseYear}-01-01&primary_release_date.lte=${currentYear}-12-31`;
+    }
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      const moviesWithDetails = await Promise.all(
+        data.results.map(async (movie) => {
+          const movieDetails = await getMovieDetails(movie.id);
+          return { ...movie, runtime: movieDetails.runtime };
+        })
+      );
+      const filteredMovies = moviesWithDetails.filter(
+        (movie) => movie.runtime >= minDuration
+      );
+
+      const sortedMovies = filteredMovies.sort(
+        (a, b) => b.vote_average - a.vote_average
+      );
+
+      displayResults(sortedMovies);
+    } catch (error) {
+      console.error("Erro ao buscar filmes:", error);
+    }
+  }
+
+  async function getMovieDetails(movieId) {
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=pt-BR`
+      );
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Erro ao obter detalhes do filme:", error);
+    }
+  }
+
+  function displayResults(movies) {
+    resultsDiv.innerHTML = "";
+    if (movies.length === 0) {
+      resultsDiv.innerHTML = "<p>Nenhum filme encontrado.</p>";
+    } else {
+      movies.forEach((movie) => {
+        const movieElement = document.createElement("div");
+        movieElement.classList.add("movie");
+        movieElement.innerHTML = `
+                  <img src="${imageBaseUrl + movie.poster_path}" alt="${
+          movie.title
+        } poster">
+                  <div>
+                      <h2>${movie.title}</h2>
+                      <p>Nota: ${movie.vote_average}</p>
+                      <p>Ano: ${new Date(movie.release_date).getFullYear()}</p>
+                      <p>Duração: ${movie.runtime} minutos</p>
+                  </div>
+              `;
+        resultsDiv.appendChild(movieElement);
+      });
+    }
+  }
+
+  getGenres();
+  form.addEventListener("submit", searchMovies);
+
   function checkAuth() {
     const token = localStorage.getItem("token");
     const username = localStorage.getItem("username");
